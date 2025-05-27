269eeb41b01ae43a6e78fc5ba0fac3c0c9c83ced
Tony Huerto <tonylex12@gmail.com>
2025-05-27 09:47:55 -0500

    ADD: Database Setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => console.log("Server running on port 5001"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Password must be at least 6 characters"],
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
